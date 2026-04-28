d009af30f733b5b0f1ebca3d6fa6f5d81d980603
manmeet277 <158315799+manmeet277@users.noreply.github.com>
2026-04-28 23:36:55 +0530

    Update API endpoint in correctText function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [result, setResult] = useState("");
 
   const correctText = async () => {
-    const response = await fetch("https://05cb-136-117-6-248.ngrok-free.app/", {
+    const response = await fetch("https://df2c-34-6-36-149.ngrok-free.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
